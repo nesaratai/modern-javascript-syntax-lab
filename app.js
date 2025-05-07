8d4a3aae843bf66cbc15895bd12952ba0cafb434
nesaratai <nesaratai@gmail.com>
2025-05-06 21:15:51 -0400

    Exercise 1-6

---
+++ app.js
@@ -0,0 +1,158 @@
+
+const flavors = ['vanilla', 'chocolate', 'strawberry'];
+
+const iceCreamFlavors = flavors.map((flavor) => {
+  return `${flavor} ice cream`;
+});
+
+console.log(iceCreamFlavors); 
+// Prints: ['vanilla ice cream', 'chocolate ice cream', 'strawberry ice cream']
+
+
+
+// ! Exercise 1:
+// a. Use the `map()` method to iterate over the provided `nums` array
+//
+// b. Use the callback function of the `map()` method you wrote to
+//    create a new array where each value is multiplied by 2.
+//    Name the new array `numsTimesTwo`.
+//
+// c. Console log the new array.
+//
+// Starting code (don't modify this):
+
+
+
+const nums = [13, 87, 2, 89, 12, 4, 90, 63];
+
+// Your code here:
+
+const numsTimesTwo = nums.map((number) => {
+    return `${number*2}`
+})
+console.log(numsTimesTwo)
+console.log(`The Array of Numbers Times Two: ${numsTimesTwo}`);
+
+
+
+// ! Exercise 2:
+// a. Given the provided `pizzaToppings` array, use destructuring to extract 
+//    the first and second values and place them into variables. Name the 
+//    variable that corresponds to the first value `firstIngredient`. Name the 
+//    variable that corresponds to the second value `secondIngredient`.
+//
+// b. Console log the `firstIngredient` and `secondIngredient` variables.
+//
+// Starting code (don't modify this):
+
+const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
+
+const [firstIngredient, secondIngredient] = pizzaToppings;
+
+console.log(`First Ingredient: ${firstIngredient}`);
+console.log(`Seconde Ingredient: ${secondIngredient}`);
+// Your code here:
+
+
+
+
+// ! Exercise 3:
+// a. Given the provided `car` object, use destructuring to create two
+//    variables: `make` and `model` that will hold the respective values.
+//
+// b. Console log the `make` and `model` variables.
+//
+// Starting code (don't modify this):
+
+const car = {
+    make: 'Audi',
+    model: 'Q5',
+  };
+  
+  // Your code here:
+
+  const {make, model} = car
+
+  console.log(`Make: ${make}`);
+  console.log(`Model: ${model}`);
+
+
+
+  // ! Exercise 4: 
+// a. Duplicate the provided `morePizzaToppings` array using the spread 
+//    operator and assign it to a variable named `uncontroversialPizzaToppings`.
+// 
+// b. Console log the `uncontroversialPizzaToppings` variable.
+//
+// Starting code (don't modify this):
+
+const morePizzaToppings = ['Cheese', 'Sauce'];
+
+// Your code here:
+
+const uncontroversialPizzaToppings = morePizzaToppings
+
+console.log(uncontroversialPizzaToppings)
+
+// i want to add one more topping 
+uncontroversialPizzaToppings.push('Pepperoni')
+console.log(uncontroversialPizzaToppings)
+
+
+
+
+// ! Exercise 5:
+// a. Duplicate the provided `anotherCar` object and spread its values into a 
+//    new variable named `myCar`.
+//
+// b. Change the `make` and `model` properties of the `myCar` object to new 
+//    values.
+//
+// c. Console log both objects and observe the results.
+//
+// Starting code (don't modify this):
+
+const anotherCar = {
+    make: 'Toyota',
+    model: 'RAV4',
+  };
+  
+  // Your code here:
+  
+const myCar = {...anotherCar};
+myCar.model = 'Q5';
+myCar.make = 'Audi';
+
+console.log(anotherCar);
+
+console.log(myCar);
+
+console.log(`I change Another Car: (${anotherCar.make} ${anotherCar.model}) to My Car: (${myCar.make} ${myCar.model})`)
+
+// ! Exercise 6:
+// a. Define a variable named `propertyName` and assign a string (like 
+//    'username', 'age', or 'email') to it.
+// 
+// b. Create an object named `userProfile`. 
+// 
+// c. Use `propertyName` as a dynamic key in `userProfile`. Assign it a 
+//    relevant value.
+//
+// d. Console log the `userProfile` object to see the result.
+//
+// Your code here:
+// username
+const propertyName = 'username';
+const userProfile = {}
+
+userProfile[propertyName] = 'johnwick';
+
+console.log(userProfile);
+
+// age
+const propertyName1 = 'age'
+const userProfile1 = {};
+
+userProfile1[propertyName1] = '22'
+
+console.log('Example',userProfile1)
